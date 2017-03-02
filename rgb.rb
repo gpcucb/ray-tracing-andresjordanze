1091f15b07c7e60babfcdb52e6039208043a4382
Andres Jordan Zeballos <andresjordanze@gmail.com>
2017-03-02 19:57:29 -0400

    fixing identation

--- rgb.rb
+++ rgb.rb
@@ -1,11 +1,11 @@
 class Rgb
   attr_accessor :r
-	attr_accessor :g
-	attr_accessor :b
+  attr_accessor :g
+  attr_accessor :b
 
-	def initialize(r,g,b)
-		@r = r.to_f
-		@g = g.to_f
-		@b = b.to_f
-	end
+  def initialize(r,g,b)
+    @r = r.to_f
+    @g = g.to_f
+    @b = b.to_f
+  end
 end
