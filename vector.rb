90c84a4a3c4903f9e5e07cb8397fd700e212eecf
Andres Jordan Zeballos <andresjordanze@gmail.com>
2017-03-09 08:34:12 -0400

    adding vector and scalar product to vector class

--- vector.rb
+++ vector.rb
@@ -8,4 +8,39 @@ class Vector
     @y = y.to_f
     @z = z.to_f
   end
+
+  def show()
+    puts "(#{@x} , #{@y} , #{@z})"
+  end
+
+  # Suma de vectores
+  def plus(vector)
+    return Vector.new(@x + vector.x, @y + vector.y, @z + vector.z);
+  end
+
+  # Resta de vectores
+  def minus(vector)
+    return Vector.new(@x - vector.x, @y - vector.y, @z - vector.z);
+  end
+
+  # Módulo de un vector
+  def mod()
+    return Math.sqrt(((@x**2) + (@y**2) + (@z**2)))
+  end
+
+  # Producto vectorial
+  def vectorProduct(vector)
+    i = (@y * vector.z) - (@z * vector.y)
+    j = (@z * vector.x) - (@x * vector.z)
+    k = (@x * vector.y) - (@y * vector.x)
+    return Vector.new(i, j, k)
+  end
+
+  # Producto escalar
+  def scalarProduct(vector)
+    i = @x * vector.x
+    j = @y * vector.y
+    k = @z * vector.z
+    return (i + j + k).to_f
+  end
 end
