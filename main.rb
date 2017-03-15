25ae876dc7b35682e3336a6ecc74dd63f2bd2323
Andres Jordan Zeballos <andresjordanze@gmail.com>
2017-03-14 20:47:39 -0400

    adding new values for camera and sphere

--- main.rb
+++ main.rb
@@ -2,21 +2,30 @@ require 'rubygems'
 require_relative 'vector.rb'
 require_relative 'camera.rb'
 require_relative 'raytracer.rb'
+require_relative 'ray.rb'
+require_relative 'sphere.rb'
 
-e = Vector.new(1,2,-6)
-center = Vector.new(1,2,-4)
-up = Vector.new(0,1,0)
-fov = 39.0
-df = 1.0
+# Valores de la cámara
+e= Vector.new(278,273,-800)
+center= Vector.new(278,273,-700)
+up= Vector.new(0,1,0)
+fov= 39.31
+df=0.035
+
 nx = 640.0
 ny = 480.0
 
+# Valores de la esfera
+position = Vector.new(370,120,370)
+radius = 120
 
 camera = Camera.new(e, center, up, fov, df)
+sphere = Sphere.new(position, radius)
 
 (0...nx).each do |i|
   (0...ny).each do |j|
     dir = camera.ray_direction(i,j,nx,ny)    
-    dir.show
+    ray = Ray.new(e, dir)
+    sphere.intersection?(ray)
   end
 end
