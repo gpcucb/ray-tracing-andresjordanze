7ca525a14392baf8eee68f67299f3ba9ffc59b18
Andres Jordan Zeballos <andresjordanze@gmail.com>
2017-03-21 20:00:30 -0400

    adding triangle

---
+++ triangle.rb
@@ -0,0 +1,45 @@
+require_relative 'vector.rb'
+require_relative 'intersection.rb'
+
+class Sphere
+  attr_accessor :position, :radius, :color
+
+  def initialize(position, radius, color)
+    @position = position #center(c)
+    @radius = radius.to_f
+    @color = color
+  end
+
+  def intersection?(ray, t)
+    e = ray.position
+    d = ray.direction
+
+    e_c = e.minus(@position)
+    dd = d.num_product(2)
+
+    a = d.scalar_product(d)
+    b = dd.scalar_product(e_c)
+    c = e_c.scalar_product(e_c) - @radius ** 2
+    #puts "(A: #{a} , B: #{b} , C: #{c})"
+   
+    discriminant = b**2-(4*a*c)
+
+    return Intersection.unsuccessful if (discriminant < 0.0)
+
+    t0 = ((b * -1) - Math.sqrt(discriminant))/(2 * a)
+    t1 = ((b * -1) + Math.sqrt(discriminant))/(2 * a)
+
+    success = false
+
+    if ((t0 > 0.0) && (t0 < t))
+      t = t0
+      success = true
+    end
+    if ((t1 > 0.0) && (t1 < t))
+      t = t1
+      success = true
+    end
+
+    return Intersection.new(t, success)
+  end
+end
