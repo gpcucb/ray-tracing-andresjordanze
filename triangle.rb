0bb29b8e6a721c7bec524556dc791918bc0d8bb5
Andres Jordan Zeballos <andresjordanze@gmail.com>
2017-03-21 20:03:42 -0400

    fixing triangle class

--- triangle.rb
+++ triangle.rb
@@ -1,45 +1,43 @@
 require_relative 'vector.rb'
 require_relative 'intersection.rb'
 
-class Sphere
-  attr_accessor :position, :radius, :color
+class Triangle
 
-  def initialize(position, radius, color)
-    @position = position #center(c)
-    @radius = radius.to_f
+  attr_accessor :a,:b,:c, :color
+  def initialize(a, b, c, color)
+    @a = a#vector
+    @b = b#vector
+    @c = c#vector
     @color = color
+  end
+
+  def determinant(vec_1,vec_2,vec_3)
+    a, b, c = vec_1.x, vec_1.y, vec_1.z
+    d, e, f = vec_2.x, vec_2.y, vec_2.z
+    g, h, i = vec_3.x, vec_3.y, vec_3.z
+    return ((a*e*i) + (b*f*g) + (c*d*h) - (c*e*g) - (b*d*i) - (a*f*h))
   end
 
   def intersection?(ray, t)
     e = ray.position
     d = ray.direction
 
-    e_c = e.minus(@position)
-    dd = d.num_product(2)
+    xa_xb, xa_xc, xa_xe = @a.x - @b.x, @a.x - @c.x, @a.x - e.x
+    ya_yb, ya_yc, ya_ye = @a.y - @b.y, @a.y - @c.y, @a.y - e.y
+    za_zb, za_zc, za_ze = @a.z - @b.z, @a.z - @c.z, @a.z - e.z
+    xd, yd, zd = d.x, d.y, d.z
 
-    a = d.scalar_product(d)
-    b = dd.scalar_product(e_c)
-    c = e_c.scalar_product(e_c) - @radius ** 2
-    #puts "(A: #{a} , B: #{b} , C: #{c})"
-   
-    discriminant = b**2-(4*a*c)
 
-    return Intersection.unsuccessful if (discriminant < 0.0)
+    m = determinant(Vector.new(xa_xb, xa_xc, xd), Vector.new(ya_yb, ya_yc, yd), Vector.new(za_zb, za_zc, zd))
+    beta = determinant(Vector.new(xa_xe, xa_xc, xd), Vector.new(ya_ye, ya_yc, yd), Vector.new(za_ze, za_zc, zd)) / m
+    gama = determinant(Vector.new(xa_xb, xa_xe, xd), Vector.new(ya_yb, ya_ye, yd), Vector.new(za_zb, za_ze, zd)) / m
+    t = determinant(Vector.new(xa_xb, xa_xc, xa_xe), Vector.new(ya_yb, ya_yc, ya_ye), Vector.new(za_zb, za_zc, za_ze)) / m
 
-    t0 = ((b * -1) - Math.sqrt(discriminant))/(2 * a)
-    t1 = ((b * -1) + Math.sqrt(discriminant))/(2 * a)
-
-    success = false
-
-    if ((t0 > 0.0) && (t0 < t))
-      t = t0
+    if ((beta > 0) && (gama > 0) && ((beta + gama) < 1))
       success = true
+      return Intersection.new(t, success)
+    else
+      return Intersection.unsuccessful
     end
-    if ((t1 > 0.0) && (t1 < t))
-      t = t1
-      success = true
-    end
-
-    return Intersection.new(t, success)
   end
 end
